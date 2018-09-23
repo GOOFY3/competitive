cb5170e6689e4b665212f666f19432e160d14d18
goofy3 <aquibbaig97@gmail.com>
2018-09-23 13:22:50 +0530

    proper java code

--- sockMerchant.java
+++ sockMerchant.java
@@ -2,7 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class sockMerchant{
+
+  static int count(int n, List<Integer> myList){
+    int cnt = 0;
+    for(int i=0; i<myList.size(); i++){
+      if(myList.get(i) == n){
+        cnt = cnt + 1;
+      }
+    }
+    // System.out.println(cnt);
+    return cnt;
+  }
+
   public static void main(String args[]) throws IOException{
+    //read from keyboard
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
     String  lines = br.readLine();
@@ -10,29 +23,28 @@ public class sockMerchant{
 
     String[] strs = lines.trim().split(" ");
     List<Integer> a = new ArrayList<Integer>();
+    List<Integer> b = new ArrayList<Integer>();
     int res = 0;
-
+    
 
     //parse input string
-    for(int i=0; i<n;i++){
-      a.add(Integer.parseInt(strs[i]));
+    for(int i=0; i<n;i++) {
+    	a.add(Integer.parseInt(strs[i]));
     }
 
-    for(int i=0; i< a.size(); i++){
-      int count = 0;
-      for(int j=0;j<a.size();j++){
-        if(a.get((i))==a.get((j))){
-          count += 1;
-          a.remove(a.get((j)));
-          System.out.println(a);
-        }
-        else continue;
-
-        if(count > 1){
-          res = res + count/2;
-        }
+    //logic
+    for(int i=0; i<a.size(); i++){
+      if(!(b.contains(a.get(i)))){
+        int count = count(a.get(i), a);
+        // System.out.println(count);
+        res = res + count/2;
+        b.add(a.get(i));
+      }
+      else{
+        continue;
       }
     }
+
 
     //print out pairs
     System.out.println(res);
